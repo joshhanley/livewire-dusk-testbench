8f68fdd204d349a486132a12b5fee13b017fb775
Josh Hanley <josh@hitsystems.com.au>
2021-02-01 08:14:08 +1100

    Add dusk endpoint to Livewire Dusk service provider Livewire Dusk endpoint has been moved out of LivewireServiceProvider in commit https://github.com/livewire/livewire/commit/5177b7509b5078a3bf5a680329d29c90292a7aab This change adds it to the dusk package so it handles its own test endpoint

--- src/LivewireDuskTestbenchServiceProvider.php
+++ src/LivewireDuskTestbenchServiceProvider.php
@@ -2,6 +2,7 @@
 
 namespace LivewireDuskTestbench;
 
+use Illuminate\Support\Facades\Route;
 use Illuminate\Support\ServiceProvider;
 use Illuminate\Testing\Constraints\SeeInOrder;
 use Laravel\Dusk\Browser;
@@ -11,6 +12,12 @@ class LivewireDuskTestbenchServiceProvider extends ServiceProvider
 {
     public function boot()
     {
+        Route::get('/livewire-dusk/{component}', function ($component) {
+            $class = urldecode($component);
+
+            return app()->call(new $class());
+        })->middleware('web');
+
         Browser::macro('assertSeeInOrder', function ($selector, $contents) {
             $fullSelector = $this->resolver->format($selector);
 
